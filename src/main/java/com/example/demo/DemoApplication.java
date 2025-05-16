04a09abca0ab5c3175d6b2a7a5a543180f35b030
Brenda Garduño <Brenda.Garduno@wisetechglobal.com>
2025-05-16 10:13:59 -0600

    envio de cambio

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-//cCAMBIO DESDE BRENDA :) 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-//PRUEBA DE QUE SI MANDA EL CAMBIO yaaa
 }
